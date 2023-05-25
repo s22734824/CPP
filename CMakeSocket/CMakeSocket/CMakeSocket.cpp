931c821d54aae8b7ebf2b8b4e107e16071b5b432
s22734824 <40797645+s22734824@users.noreply.github.com>
2023-05-25 15:47:53 +0800

    EasySocket
    EasySocket In Cmake Build

---
+++ CMakeSocket/CMakeSocket/CMakeSocket.cpp
@@ -0,0 +1,18 @@
+﻿// CMakeSocket.cpp: 定義應用程式的進入點。
+//
+
+#include "CMakeSocket.h"
+#include "EasySocket.hpp"
+using namespace std;
+using namespace masesk;
+
+void handleData(const std::string& data) {
+	cout << "Client sent: " + data << endl;
+}
+
+int main()
+{
+	EasySocket socketManager;
+	socketManager.socketListen("test", 8080, &handleData);
+	return 0;
+}
