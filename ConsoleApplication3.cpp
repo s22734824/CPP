60dff626a6a2ad33cec89fc13573b92e7bae8d56
s22734824 <40797645+s22734824@users.noreply.github.com>
2023-07-21 13:41:35 +0800

    Update ConsoleApplication3.cpp
    dll import

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -1,24 +1,93 @@
-﻿// ConsoleApplication3.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+// ConsoleTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
-#include <windows.h>
+#include <Windows.h>
+//#include <D:\Project\Local\CppDllTest\ConsoleTest\ConsoleTest\DllTest.dll>
 
+// If you don't have the DLL header file, manually define the macros
+#define ASR_EXPORT __declspec(dllexport)
+#define ASR_API __cdecl
 
+typedef enum
+{
+	ESCORE_HWM_CPU_TEMP,
+	ESCORE_HWM_MB_TEMP,
+	ESCORE_HWM_CPU_FAN1_SPEED,
+	ESCORE_HWM_CPU_FAN2_SPEED,
+	ESCORE_HWM_CHASSIS_FAN1_SPEED,
+	ESCORE_HWM_CHASSIS_FAN2_SPEED,
+	ESCORE_HWM_CHASSIS_FAN3_SPEED,
+	ESCORE_HWM_CHASSIS_FAN4_SPEED,
+	ESCORE_HWM_SB_FAN1_SPEED,
+	ESCORE_HWM_POWER_FAN_SPEED,
+	ESCORE_HWM_CPU_FAN12_SHARE_SPEED,
+	ESCORE_HWM_CHASSIS_FAN12_SHARE_SPEED,
+	ESCORE_HWM_VCOREV,
+	ESCORE_HWM_5V,
+	ESCORE_HWM_12V,
+	ESCORE_HWM_3P3V,
+	ESCORE_HWM_CPU_INPUTV,
+	ESCORE_HWM_VIN,
+	ESCORE_HWM_CARDSIDE_TEMP,
+	ESCORE_HWM_FRNT_FAN1_SPEED,
+	ESCORE_HWM_FRNT_FAN2_SPEED,
+	ESCORE_HWM_FRNT_FAN3_SPEED,
+	ESCORE_HWM_VDDCR_SOCV,
+	ESCORE_HWM_DRAMV,
+	ESCORE_HWM_VDD_MISCV,
+	ESCORE_HWM_VCCIOV,
+	ESCORE_HWM_VCCSAV,
+	ESCORE_HWM_VCCSTSFRV,
+	ESCORE_HWM_3VSBV,
+	ESCORE_HWM_VBATV,
+	ESCORE_HWM_TR1_TEMP,
+	ESCORE_HWM_DCIN,
+	ESCORE_HWM_GPU_FAN_SPEED,
+	ESCORE_HWM_SYSTEM_FAN1_SPEED,
+	ESCORE_HWM_3V,
+	ESCORE_HWM_CPU_SPEED,
+} ESCORE_HWM_ITEM;
 
+extern "C" bool ASR_API AsrLibGetHardwareMonitor(ESCORE_HWM_ITEM Item, double* Value);
+extern "C" bool ASR_API AsrLibDllInit();
 
 
 int main()
 {
-    HINSTANCE hGetProcIDDLL = LoadLibrary(L"C:\\Users\\peter\\Desktop\\FanControl\\AsrCore.dll");
-    if (!hGetProcIDDLL)
-    {
+	std::cout << "Hello World!\n";
 
-    }
-    else
-    {
-        std::cout << "Hello World!\n";
-    }
+	HMODULE dllHandle = LoadLibrary(L"C:\\Users\\iBUYPOWER\\Desktop\\AsrCoreAPI_Tuning_B760M_C_B760M_PRO_RS_20230717\\AsrCoreAPI_Tuning_B760M_C_B760M_PRO_RS\\AsrCore.dll");
+	if (dllHandle != nullptr) {
+
+		typedef bool(ASR_API* InitDll)();
+		InitDll initDll = reinterpret_cast<InitDll>(GetProcAddress(dllHandle, "AsrLibDllInit"));
+
+		bool res = initDll();
+
+		// Step 2: Get the function address
+		typedef bool(ASR_API* GetMonitorFunc)(ESCORE_HWM_ITEM Item, double* Value);
+		GetMonitorFunc getHardwareMonitor = reinterpret_cast<GetMonitorFunc>(GetProcAddress(dllHandle, "AsrLibGetHardwareMonitor"));
+		double value = -1;
+		if (getHardwareMonitor != nullptr) {
+			// Step 3: Call the function
+			double* address = &value;
+			bool success = getHardwareMonitor(ESCORE_HWM_ITEM::ESCORE_HWM_CPU_TEMP, &value);
+			// Step 4: Print the result
+			std::cout << "Result from Print: " << value << std::endl;
+		}
+		else {
+			std::cout << "Failed to get the function address." << std::endl;
+		}
+		// Step 5: Unload the DLL
+		FreeLibrary(dllHandle);
+	}
+	else {
+		std::cout << "Failed to load the DLL." << std::endl;
+	}
+
+	return 0;
+
 }
 
 // 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
